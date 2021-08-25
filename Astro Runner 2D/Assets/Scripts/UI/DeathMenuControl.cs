d7528552262e8520adfca10a7544a9c5dc967d74
marshallein <maxboiz123hn@gmail.com>
2021-08-25 22:24:35 +0700

    adjust some change to The Game

--- Astro Runner 2D/Assets/Scripts/UI/DeathMenuControl.cs
+++ Astro Runner 2D/Assets/Scripts/UI/DeathMenuControl.cs
@@ -1,20 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DeathMenuControll : MonoBehaviour
+public class DeathMenuControl: MonoBehaviour
 {
 
     public string mainMenu;
 
     public void restartGame()
     {
-
+        FindObjectOfType<GameManager>().ResetScene();
     }
 
     public void QuitToMain()
     {
-
+        SceneManager.LoadScene(mainMenu);
     }
 
 
